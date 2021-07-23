2acb703fe1ce4fc4ef1d7c826da2db820f1dc3ea
Archit Raj <arch10@ymail.com>
2021-07-24 03:46:30 +0530

    fixed analytics remote ip unknown

--- src/service/analyticsService.js
+++ src/service/analyticsService.js
@@ -1,9 +1,9 @@
 const { Analytics, validator } = require('../models/Analytics');
 const { logger } = require('../util');
 
-const saveAnalytics = async (urlCode, browser, os, ip) => {
+const saveAnalytics = async (urlCode, browser, os, remoteIp) => {
   try {
-    const { value, error } = validator({ urlCode, browser, os, ip });
+    const { value, error } = validator({ urlCode, browser, os, remoteIp });
     if (error) {
       throw new Error(error.details[0].message);
     }
